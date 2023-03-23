fe4aca322834489123d7304da1814d1e3b937660
jagatheswaran.m <jagatheswaranmj@gmail.com>
2023-03-23 19:52:34 +0530

    Initial commit

---
+++ Interview_programs/Second Most repeated character.py
@@ -0,0 +1,12 @@
+s="jajgatthestwaran"
+k={}
+
+for i in s:
+    if i in  k:
+        k[i]=k[i]+1
+
+    else:
+        k[i]=1
+
+r=sorted(k, key=k.get, reverse=True)
+print("second most repated" , r[1])
