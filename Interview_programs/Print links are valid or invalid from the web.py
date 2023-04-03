9a501ee3a3232f136ecc58cc01921ce5afa538b8
jagatheswaran.m <jagatheswaranmj@gmail.com>
2023-04-03 18:45:13 +0530

    new

--- Interview_programs/Print links are valid or invalid from the web.py
+++ Interview_programs/Print links are valid or invalid from the web.py
@@ -11,7 +11,6 @@ from selenium.webdriver.support.ui import Select
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
-
 driver = webdriver.Chrome(executable_path='C:/Users/jagatheswaran.m/PycharmProjects/pythonProject3/chromedriver.exe')
 #driver = webdriver.Edge(executable_path='C:/Users/jagatheswaran.m/PycharmProjects/pythonProject/msedgedriver.exe')
 driver.maximize_window()
