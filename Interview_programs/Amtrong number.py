342f33de9161433b8df33ba88da0f6eb177a1fad
jagatheswaran.m <jagatheswaranmj@gmail.com>
2023-04-04 16:16:26 +0530

    new

--- Interview_programs/Amtrong number.py
+++ Interview_programs/Amtrong number.py
@@ -1,12 +1,14 @@
-a=int(input("eneter num"))
-sum=0
+a=int(input("enter num \n"))
 temp=a
+sum=0
+
 while temp>0:
     digit=temp%10
     sum=sum+(digit**3)
     temp=temp//10
 
 if a==sum:
-    print("yes")
+    print("Yes amstrong")
+
 else:
     print("no")
