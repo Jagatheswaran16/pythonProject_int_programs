2b4da78d625f3f12fd645e849dfaa343df61e387
simple <jagatheswaran555@gmail.com>
2023-04-11 14:50:45 +0530

    new chnage123

--- Interview_programs/Patterns.py
+++ Interview_programs/Patterns.py
@@ -1,12 +1,7 @@
 def test(n):
     for i in range(0, n):
         for j in range(0, i+1):
-
-            print("10", end="")
+            print("#", end="")
         print("\r")
 
 test(6)
-print("jaga chnages")
-
-
-
